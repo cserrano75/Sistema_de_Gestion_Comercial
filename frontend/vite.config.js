50f79b53af43e58c8c6aa57986e75a0158878cda
Claudio Serrano <claudio.serrano.rojas@gmail.com>
2026-05-15 20:28:07 -0400

    Forzamos el acceso a un unico puerto

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  server: {
+    port: 5173,       // Fuerza a Vite a usar siempre el puerto 5173
+    strictPort: true, // Si el 5173 está ocupado por un proceso colgado, da un error en vez de saltar al 5174
+  },
 })
